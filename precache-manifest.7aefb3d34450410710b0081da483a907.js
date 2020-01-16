e547e0937f5678b2683df6995ab2ecc0b6da887c
Antoni Petrenko <flay9123@gmail.com>
2020-01-16 19:19:33 +0200

    Updates

---
+++ precache-manifest.7aefb3d34450410710b0081da483a907.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc8ebc811c6fe5e41a5ea41b0bd16bf",
+    "url": "/API-to-do/index.html"
+  },
+  {
+    "revision": "1543c46f7280ba448844",
+    "url": "/API-to-do/static/css/main.9901b213.chunk.css"
+  },
+  {
+    "revision": "9e9b2ea4a4074d7efb76",
+    "url": "/API-to-do/static/js/2.35f4212e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/API-to-do/static/js/2.35f4212e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1543c46f7280ba448844",
+    "url": "/API-to-do/static/js/main.1447c6d2.chunk.js"
+  },
+  {
+    "revision": "0bfc73a084d58ebf8bc4",
+    "url": "/API-to-do/static/js/runtime-main.87f36fee.js"
+  }
+]);
