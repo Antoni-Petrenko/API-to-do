c2c63277eee40ab945b4de79669d91074e0461a8
Antoni Petrenko <flay9123@gmail.com>
2020-01-17 15:11:05 +0200

    delete comment

--- src/index.js
+++ src/index.js
@@ -6,16 +6,7 @@ import {Provider} from 'react-redux';
 import reducer from './store/reducer';
 import thunk from 'redux-thunk';
 
-//If you want to use ReduxDevTools import compose from redux----
-//                                                              |
-//                                                              V
-// const composeEnhancers =
-//   typeof window === 'object' &&
-//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
-//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-//       // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-//     }) : compose;
-// const store=createStore(reducer,composeEnhancers(applyMiddleware(thunk)));
+
 
 const store=createStore(reducer,applyMiddleware(thunk));
 const app=(
